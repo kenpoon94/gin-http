081402bfb589315c508187bcaaaafb18eab6ff23
ken.poon <ken@cyder.com.sg>
2021-06-06 15:24:50 +0800

    Add user hander routes

--- httpd/gohttp.go
+++ httpd/gohttp.go
@@ -1,19 +1,24 @@
 package main
 
 import (
-	"example.com/gohttp/httpd/handler"
-	"example.com/gohttp/platform/newsfeed"
+	"example.com/gin-http/httpd/handler"
+	"example.com/gin-http/platform/newsfeed"
+	"example.com/gin-http/platform/user"
 	"github.com/gin-gonic/gin"
 )
 
 
 func main() {
 	feed := newsfeed.New()
+	user := user.New()
 	r := gin.Default()
 
 	r.GET("/ping", handler.PingGet())
 	r.GET("/newsfeed", handler.NewsFeedGet(feed))
 	r.POST("/newsfeed", handler.NewsFeedPost(feed))
+	r.GET("/user", handler.GetAllUsers(user))
+	r.GET("/user/:username", handler.GetUser(user))
+	r.POST("/user/new", handler.CreateAccount(user))
 
 	r.Run(":8080")
 	
